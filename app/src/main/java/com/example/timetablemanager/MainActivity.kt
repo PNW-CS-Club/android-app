ccdb23a54215faeb4f513ef64d5091945e62fe96
LUIS <alvearluis@hotmail.es>
2022-10-04 22:06:53 +0200

    Created new class "NewActivity" for the new window after clicking the button in "MainActivity". Added calendar, input box a button with onclick feature to new_activity.xml

--- app/src/main/java/com/example/timetablemanager/MainActivity.kt
+++ app/src/main/java/com/example/timetablemanager/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.timetablemanager
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonClick = findViewById<Button>(R.id.button_id)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, NewActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
